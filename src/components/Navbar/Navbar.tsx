350b889cb125d04d6a290f2aeeeb6ca6be480132
Zaid <syedzaidahmed21@gmail.com>
2024-04-28 15:10:40 +0500

    error loading and not-found added

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,31 +2,30 @@ import Link from "next/link";
 
 interface Ilink {
   href: string;
-  rel: string;
+  title: string;
 }
 const link: Ilink[] = [
-  { href: "/", rel: "Home" },
-  { href: "/about", rel: "About" },
-  { href: "/contact", rel: "Contact" },
-  { href: "/blog", rel: "Blog" },
-  { href: "/admin", rel: "Admin" },
+  { href: "/", title: "Home" },
+  { href: "/about", title: "About" },
+  { href: "/contact", title: "Contact" },
+  { href: "/blog", title: "Blog" },
+  { href: "/admin", title: "Admin" },
 ];
 const Navbar = () => {
+  const NavItems = link.map((item: Ilink) => {
+    return (
+      <li key={item.href} className="text-lg text-gray-500">
+        <Link href={item.href}>{item.title}</Link>
+      </li>
+    );
+  });
   return (
     <div>
       <div>
         <h1>Navbar</h1>
       </div>
       <div>
-        <ul className="bg-red-300 flex gap-6 justify-center">
-          {link.map((item: Ilink) => {
-            return (
-              <li key={item.href} className="text-lg text-gray-500">
-                <Link href={item.href}>{item.rel}</Link>
-              </li>
-            );
-          })}
-        </ul>
+        <ul className="bg-red-300 flex gap-6 justify-center">{NavItems}</ul>
       </div>
     </div>
   );
