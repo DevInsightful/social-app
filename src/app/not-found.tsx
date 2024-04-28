8a858cbee13ca20a28d94fe938432362e1d3020b
Zaid <syedzaidahmed21@gmail.com>
2024-04-28 20:30:04 +0500

    css addedd

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,14 @@
+import Link from "next/link";
 const NotFound = () => {
-  return <div>Not-found</div>;
+  return (
+    <div className="bg-gray-700 h-[90vh] flex justify-center items-center flex-col gap-5">
+      <h1 className="text-2xl font-bold">Not-found</h1>
+      <h6>Sorry the page you are looking for does not exist</h6>
+      <Link href="/" className="border-b-2 border-gray-400">
+        Back To Home
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
