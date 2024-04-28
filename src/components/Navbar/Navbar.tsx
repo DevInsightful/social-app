d28fb74f2f9aca0c2714622d414b6e39213d19be
Zaid <syedzaidahmed21@gmail.com>
2024-04-28 14:51:01 +0500

    routing set nav working

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,35 @@
+import Link from "next/link";
+
+interface Ilink {
+  href: string;
+  rel: string;
+}
+const link: Ilink[] = [
+  { href: "/", rel: "Home" },
+  { href: "/about", rel: "About" },
+  { href: "/contact", rel: "Contact" },
+  { href: "/blog", rel: "Blog" },
+  { href: "/admin", rel: "Admin" },
+];
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <div>
+        <h1>Navbar</h1>
+      </div>
+      <div>
+        <ul className="bg-red-300 flex gap-6 justify-center">
+          {link.map((item: Ilink) => {
+            return (
+              <li key={item.href} className="text-lg text-gray-500">
+                <Link href={item.href}>{item.rel}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
